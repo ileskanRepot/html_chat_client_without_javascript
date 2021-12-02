03c578575560d029c3544e292ebf7cf7e8682f93
Ileska <veksepoli@gmail.com>
2021-12-02 14:01:01 +0200

    Make form functioning

--- app.py
+++ app.py
@@ -10,14 +10,15 @@ print("Serving HTTP on port {PORT} ...")
 while True:
 	client_connection, client_address = listen_socket.accept()
 	request_data = client_connection.recv(1024)
-	print("\nrequest_data.decode('utf-8'):",request_data.decode('utf-8'))
+	print("\nrequest_data.decode(''utf-8\'):\'"+request_data.decode('utf-8')+"\'\n")
 
 	filename = 'noURL.html'
-
-	print('\''+request_data.decode('utf-8')[5]+'\'')
-	if request_data.decode('utf-8')[5] == '?':
-		print("Kyssäri")
-		filename = request_data.decode('utf-8').split(' ')[1].split('?')[1]
+	print("client_address: ", client_address)
+	
+	print("request_data.decode('utf-8').split(" ")[1]:'"+request_data.decode('utf-8').split(" ")[1].replace("?", "")+"'")
+	if request_data.decode('utf-8').split(" ")[1][0] == '/':
+		print("Kautta")
+		filename = request_data.decode('utf-8').split(' ')[1].split('/')[1].replace("?", "")
 		print(filename)
 
 	if filename == 'noURL.html':
@@ -28,10 +29,22 @@ while True:
 			if ".." in filename:
 				file = open('nono.html')
 				http_response = bytes("HTTP/1.1 200 OK\n\n"+(file.read()),'utf-8')
+				file.close()
+
 			else:
-				file = open('chats/'+filename.split('/')[0])
-				http_response = bytes("HTTP/1.1 200 OK\n\n"+(file.read()),'utf-8')
-			file.close()
+				fileChats = open('chats/'+filename.split('/')[0], "r")
+				fileUpperHalf = open('upperHalf', "r")
+				fileBottomHalf = open('bottomHalf', "r")
+				
+				chats = fileChats.read().replace("\n", "<br>")
+				upperHalf = fileUpperHalf.read()
+				bottomHalf = fileBottomHalf.read()
+
+				http_response = bytes("HTTP/1.1 200 OK\n\n"+upperHalf+chats+bottomHalf,'utf-8')
+				print("chats.read():'"+chats+'\'')
+				fileChats.close()
+				fileUpperHalf.close()
+				fileBottomHalf.close()
 		except IOError:
 			http_response = b"""\
 HTTP/1.1 200 OK
@@ -46,5 +59,4 @@ HTTP/1.1 200 OK
 </html>
 """
 	client_connection.sendall(http_response)
-#	request_data = client_connection.recv(1024)
 	client_connection.close()
